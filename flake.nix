9cd262b6842cb7bd0b2686f764b68ff0632657eb
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-04-07 11:12:48 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rv32_pkgs = nixpkgs.legacyPackages.${system}.pkgsCross.riscv32-embedded;
+        rv64_pkgs = nixpkgs.legacyPackages.${system}.pkgsCross.riscv64-embedded;
+        rust_nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override { extensions = [ "rust-src" ]; });
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = with pkgs; [
+              rust_nightly
+              rv32_pkgs.buildPackages.gdb
+              rv32_pkgs.buildPackages.gcc
+              rv32_pkgs.buildPackages.binutils
+              # rv64_pkgs.buildPackages.gdb
+              # rv64_pkgs.buildPackages.binutils
+              rust-bindgen
+              # llvmPackages.libclang
+              # libclang
+
+              llvmPackages.clang
+              qemu
+              bacon
+              rust-analyzer
+            ];
+
+            shellHook = "";
+
+            env = {
+              RUST_GDB =
+                "${rv32_pkgs.buildPackages.gdb}/bin/riscv32-none-elf-gdb";
+              # LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+              LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+              # INCLUDE = "${llvmPackages.clang}/resource-root/include/";
+            };
+          };
+      });
+}
---
+++ src/bindings.rs
@@ -0,0 +1,77 @@
+pub const MAGIC_STAGE: &[u8; 4] = b"stg\0";
+pub const MAGIC_CONNECTOR: &[u8; 4] = b"con\0";
+
+pub type ssd_os_ctrl_fn = ::core::option::Option<unsafe extern "C" fn() -> ::core::ffi::c_int>;
+pub type ssd_os_stage_fn = ::core::option::Option<
+    unsafe extern "C" fn(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void,
+>;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct lring_entry {
+    pub function: *mut ::core::ffi::c_void,
+    pub ctx: *mut ::core::ffi::c_void,
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_print_s(s: *const ::core::ffi::c_char);
+}
+
+pub type ssd_os_conn_fn =
+    ::core::option::Option<unsafe extern "C" fn(entry: *mut lring_entry) -> *mut pipeline>;
+pub type ssd_os_conn_ring_fn =
+    ::core::option::Option<unsafe extern "C" fn(entry: *mut lring_entry) -> ::core::ffi::c_int>;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct stage {
+    pub magic: [::core::ffi::c_char; 4usize],
+    pub name: [::core::ffi::c_char; 32usize],
+    pub init_fn: ssd_os_ctrl_fn,
+    pub exit_fn: ssd_os_ctrl_fn,
+    pub stage_fn: ssd_os_stage_fn,
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_print_lock();
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_print_unlock();
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_print_ss(s1: *const ::core::ffi::c_char, s2: *const ::core::ffi::c_char);
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct pipeline {
+    pub name: [::core::ffi::c_char; 32usize],
+    pub internal: [*mut ::core::ffi::c_void; 2usize],
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct connector {
+    pub magic: [::core::ffi::c_char; 4usize],
+    pub name: [::core::ffi::c_char; 32usize],
+    pub init_fn: ssd_os_ctrl_fn,
+    pub exit_fn: ssd_os_ctrl_fn,
+    pub conn_fn: ssd_os_conn_fn,
+    pub ring_fn: ssd_os_conn_ring_fn,
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_sleep(sec: ::core::ffi::c_int);
+}
+
+unsafe extern "C" {
+    pub fn ssd_os_get_connection(
+        connector_name: *mut ::core::ffi::c_char,
+        pipe_name: *mut ::core::ffi::c_char,
+    ) -> *mut pipeline;
+}
+unsafe extern "C" {
+    pub fn ssd_os_print_i(x: ::core::ffi::c_ulong);
+}
