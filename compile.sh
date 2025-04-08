9257f67427e182cf51ffd0d37d259956440b6b2b
birkj <jens.birk.andersen@gmail.com>
2025-04-08 09:58:27 +0200

    rename to rs_ftl

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cargo b --profile small
 ar x ./target/target/small/libftl_ssd_os.a
-find . -type f -name "ftl_ssd_os*.o" -exec cp {} ../ssd_os/app/programs/build_rs/ftl_ssd_os.o \;
-cp ftl_ssd_os.conn ../ssd_os/app/programs/ftl_ssd_os.conn
-cp ftl_ssd_os.pipe ../ssd_os/app/programs/ftl_ssd_os.pipe
+find . -type f -name "ftl_ssd_os*.o" -exec cp {} ../ssd_os/app/programs/build_rs/rs_ftl.o \;
+cp ftl_ssd_os.conn ../ssd_os/app/programs/rs_ftl.conn
+cp ftl_ssd_os.pipe ../ssd_os/app/programs/rs_ftl.pipe
