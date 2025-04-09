cfa3cb71c9b17c63d9a32fd78aa84c4d2f4ab248
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-04-09 11:26:40 +0200

    add initial bbt

--- src/bbt.rs
+++ src/bbt.rs
@@ -1,16 +1,69 @@
+/// ASSUMPTIONS:
+/// We assume that bbt is static, as such we use static lifetimes.
+/// we assume the structure is channels[LUNS[Planes[Blocks]]]
+use alloc::vec::Vec;
+
+use crate::{bindings::nvm_mmgr_geometry, core::addresses::PhysicalBlockAddress};
+
 pub struct BadBlockTable {
-    // Channels: &'static[u8]
-    Channels: [u8;2]
+    pub channel_bbts: Vec<ChannelBadBlockTable>,
 }
-
-
 impl BadBlockTable {
-    pub fn new() -> Self {
+    pub fn new(geometry: &nvm_mmgr_geometry) -> Self {
         BadBlockTable {
-            Channels: [42, 42] 
+            channel_bbts: Vec::with_capacity(geometry.n_of_ch as usize),
         }
     }
-    pub fn read(&self) -> u8 {
-        self.Channels[0]
+
+    pub fn set_bad_block(&mut self, pba: PhysicalBlockAddress) {
+        self.channel_bbts[pba.channel as usize].lun_bbts[pba.lun as usize].plane_bbts
+            [pba.plane as usize]
+            .bbt[pba.block as usize] = BadBlockStatus::Bad;
     }
+
+    pub fn get_block_status(&mut self, pba: PhysicalBlockAddress) -> BadBlockStatus {
+        self.channel_bbts[pba.channel as usize].lun_bbts[pba.lun as usize].plane_bbts
+            [pba.plane as usize]
+            .bbt[pba.block as usize]
+    }
+}
+
+pub struct ChannelBadBlockTable {
+    pub(self) lun_bbts: Vec<LunBadBlockTable>,
+}
+impl ChannelBadBlockTable {
+    fn new(geometry: &nvm_mmgr_geometry) -> Self {
+        Self {
+            lun_bbts: Vec::with_capacity(geometry.lun_per_ch as usize),
+        }
+    }
+}
+
+pub struct LunBadBlockTable {
+    pub(self) plane_bbts: Vec<PlaneBadBlockTable>,
+}
+impl LunBadBlockTable {
+    fn new(geometry: &nvm_mmgr_geometry) -> Self {
+        Self {
+            plane_bbts: Vec::with_capacity(geometry.n_of_planes as usize),
+        }
+    }
+}
+
+pub struct PlaneBadBlockTable {
+    pub(self) bbt: Vec<BadBlockStatus>,
+}
+impl PlaneBadBlockTable {
+    fn new(geometry: &nvm_mmgr_geometry) -> Self {
+        Self {
+            bbt: Vec::with_capacity(geometry.blk_per_lun as usize),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum BadBlockStatus {
+    Bad,
+    Good,
+    Reserved,
 }
---
+++ src/core/addresses.rs
@@ -0,0 +1,55 @@
+use crate::bindings::nvm_ppa_addr;
+
+pub struct PhysicalBlockAddress {
+    pub channel: u16,
+    pub lun: u8,
+    pub plane: u8,
+    pub block: u16,
+}
+
+impl From<nvm_ppa_addr> for PhysicalBlockAddress {
+    fn from(value: nvm_ppa_addr) -> Self {
+        PhysicalBlockAddress {
+            channel: value.get_channel(),
+            lun: value.get_lun(),
+            plane: value.get_lun(),
+            block: value.get_block(),
+        }
+    }
+}
+
+/// ASSUMES THE FOLLOWING STRUCTURE
+/// struct nvm_ppa_addr {
+///     /* Generic structure for all addresses */
+///     union {
+///         struct {
+///             uint64_t sec   : 3;
+///             uint64_t pl    : 2;
+///             uint64_t ch    : 12;
+///             uint64_t lun   : 6;
+///             uint64_t pg    : 12;
+///             uint64_t blk   : 15;
+///             uint64_t rsv   : 14;
+///         } g;
+///
+///         uint64_t ppa;
+///     };
+/// };
+///
+impl nvm_ppa_addr {
+    pub fn get_channel(self) -> u16 {
+        ((unsafe { self.ppa } >> 5) & 0b1111_1111_1111) as u16
+    }
+
+    pub fn get_plane(&self) -> u8 {
+        ((unsafe { self.ppa } >> 3) & 0b0011) as u8
+    }
+
+    pub fn get_lun(&self) -> u8 {
+        ((unsafe { self.ppa } >> 17) & 0b0011_1111) as u8
+    }
+
+    pub fn get_block(&self) -> u16 {
+        ((unsafe { self.ppa } >> 35) & 0b0111_1111_1111_1111) as u16
+    }
+}
