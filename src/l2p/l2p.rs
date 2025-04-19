376b424b749c482c98f2677033192939c39aa509
birkj <jens.birk.andersen@gmail.com>
2025-04-19 15:24:53 +0200

    add l2p

---
+++ src/l2p/l2p.rs
@@ -0,0 +1,11 @@
+use crate::bindings::generated::{nvm_ppa_addr, nvm_ppa_addr__bindgen_ty_1};
+
+const test : nvm_ppa_addr = nvm_ppa_addr{
+    __bindgen_anon_1: nvm_ppa_addr__bindgen_ty_1 {
+        ppa: 0,
+    }
+};
+
+pub struct L2pMapper {
+    
+}
