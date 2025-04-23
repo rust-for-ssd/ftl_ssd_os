6d8b416ea6ede60386e3fa715ac7d6133b970af4
Malthe Larsen <75012728+MLFlexer@users.noreply.github.com>
2025-04-23 16:52:52 +0200

    Update mod.rs

--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -1,4 +1,4 @@
-// #[cfg(feature = "connector_per_component")]
+#[cfg(feature = "connector_per_component")]
 pub mod connector_per_component;
 #[cfg(feature = "pipeline_per_cmd")]
 pub mod pipeline_per_cmd;
