5a37aa1c741ebba0c7b5d3d6c57c5a7463cefb00
birkj <jens.birk.andersen@gmail.com>
2025-04-23 14:30:48 +0200

    cannot find mm

--- src/media_manager/media_manager.rs
+++ src/media_manager/media_manager.rs
@@ -1,15 +1,37 @@
 use crate::{
-    l2p::l2p::PhysicalAddr,
-    println,
-    requester::requester::{CommandType, Request},
+    bindings::generated::nvm_mmgr_geometry, l2p::l2p::PhysicalAddr, println, requester::requester::{CommandType, Request}
 };
 use alloc::{collections::BTreeMap, vec::Vec};
 use core::{alloc::Allocator, ptr::null_mut};
 
 pub type mm_page = [u8; 2];
 
+
+// TODO, change relevant to u64
+#[derive(Debug)]
+pub struct Geometry { 
+    pub n_pages: u32, //TODO convert to u64 when possible
+    pub n_of_ch: u8,
+    pub lun_per_ch: u8,
+    pub blk_per_lun: u16,
+    pub pg_per_blk: u16
+}
+
+impl Geometry {
+    pub fn map_geometry(nvm_geo: &nvm_mmgr_geometry) -> Geometry {
+        Geometry {
+            n_pages: nvm_geo.tot_pg as u32, //TODO convert to u64 when possible
+            n_of_ch: nvm_geo.n_of_ch,
+            lun_per_ch: nvm_geo.lun_per_ch, 
+            blk_per_lun: nvm_geo.blk_per_lun,
+            pg_per_blk: nvm_geo.pg_per_blk, 
+        }
+    }
+}
+
 pub struct MediaManager<A: Allocator + 'static> {
     data_buffer: BTreeMap<PhysicalAddr, mm_page, &'static A>,
+    pub geometry: Geometry
 }
 
 #[derive(Debug)]
@@ -20,9 +42,10 @@ pub enum MM_ERR {
 }
 
 impl<A: Allocator + 'static> MediaManager<A> {
-    pub fn new(alloc: &'static A) -> Self {
+    pub fn new(alloc: &'static A, geo: Geometry) -> Self {
         MediaManager {
             data_buffer: BTreeMap::new_in(alloc),
+            geometry: geo
         }
     }
 
