b669b2d258cfdd5eec6291686ccdc1ee3c153921
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-04-30 14:20:55 +0200

    cleaned pipeline_per_cmd

--- src/apps/pipeline_per_cmd/connectors/requester.rs
+++ src/apps/pipeline_per_cmd/connectors/requester.rs
@@ -1,22 +1,21 @@
-use core::ptr::{null, null_mut};
-
-use alloc::vec::Vec;
+use core::ptr::null_mut;
 
 use crate::{
-    allocator::sdd_os_alloc::SimpleAllocator, bindings::{
+    allocator::sdd_os_alloc::SimpleAllocator,
+    bindings::{
         generated::{lring_entry, pipeline},
-        lring::{LRing, LRingErr},
         mem::MemoryRegion,
-        safe::{ssd_os_get_connection, ssd_os_sleep},
-    }, make_connector_static, media_manager::media_manager::mm_page, println, requester::requester::{RequestWorkloadGenerator, Status, WorkloadType}, shared::core_local_cell::CoreLocalCell
+        safe::ssd_os_get_connection,
+    },
+    make_connector_static, println,
+    requester::requester::RequestWorkloadGenerator,
+    shared::core_local_cell::CoreLocalCell,
 };
 
-use crate::requester::requester::{Request, RequestError, CommandType};
-
+use crate::requester::requester::{CommandType, Request};
 
 make_connector_static!(requester1, init, exit, pipe_start, ring);
 
-// static lring: LRing<128> = LRing::new();
 static ALLOC: SimpleAllocator = SimpleAllocator::new();
 pub static WORKLOAD_GENERATOR: CoreLocalCell<RequestWorkloadGenerator<SimpleAllocator>> =
     CoreLocalCell::new();
@@ -27,49 +26,35 @@ static mut READ_PIPE: *mut pipeline = core::ptr::null_mut();
 static mut WRITE_PIPE: *mut pipeline = core::ptr::null_mut();
 
 fn init() -> ::core::ffi::c_int {
-    println!("REQUESTER_INIT");
     let mem_region = MemoryRegion::new_from_cpu(1);
-    // let Ok(()) = lring.init(c"REQUESTER_LRING", mem_region.free_start, 0) else {
-    //     panic!("REQUESTER_LRING WAS ALREADY INITIALIZED!");
-    // };
-    // let ring = lring.get_lring().unwrap();
-    // mem_region.reserve(ring.alloc_mem as usize);
-
-    println!("LRING_INIT");
     ALLOC.initialize(mem_region.free_start.cast(), mem_region.end.cast());
-    println!("{:?}", mem_region.free_start);
-    println!("{:?}", mem_region.end);
-    
-    
-    unsafe { READ_PIPE = ssd_os_get_connection(c"requester1", c"read"); };
-    unsafe { WRITE_PIPE = ssd_os_get_connection(c"requester1", c"write"); };
 
-    
-    
-    // #[cfg(feature = "benchmark")]
-    // {
-    WORKLOAD_GENERATOR.set(RequestWorkloadGenerator::new(
-        WorkloadType::WRITE,
-        N_REQUESTS,
-        &ALLOC,
-    ));
-    let workload = WORKLOAD_GENERATOR.get_mut();
-    workload.init_workload();  
-    // }
+    unsafe {
+        READ_PIPE = ssd_os_get_connection(c"requester1", c"read");
+    };
+    unsafe {
+        WRITE_PIPE = ssd_os_get_connection(c"requester1", c"write");
+    };
+
+    #[cfg(feature = "benchmark")]
+    {
+        WORKLOAD_GENERATOR.set(RequestWorkloadGenerator::new(
+            crate::requester::requester::WorkloadType::WRITE,
+            N_REQUESTS,
+            &ALLOC,
+        ));
+        let workload = WORKLOAD_GENERATOR.get_mut();
+        workload.init_workload();
+    }
     0
 }
 
 fn exit() -> ::core::ffi::c_int {
-    println!("EXIT!");
     0
 }
 
 fn pipe_start(entry: *mut lring_entry) -> *mut pipeline {
-    #[cfg(feature = "debug")]
-    ssd_os_sleep(1);
-
     let Some(entry) = lring_entry::new(entry) else {
-        println!("NULL PTR!");
         return null_mut();
     };
 
@@ -84,18 +69,16 @@ fn pipe_start(entry: *mut lring_entry) -> *mut pipeline {
             match req.cmd {
                 CommandType::READ => {
                     entry.set_ctx(req);
-                    // println!("HERE1");
                     return ssd_os_get_connection(c"requester1", c"read");
-                },
+                }
                 CommandType::WRITE => {
-                    // println!("HERE2");
                     entry.set_ctx(req);
-                    return ssd_os_get_connection(c"requester1", c"write")
-                },
+                    return ssd_os_get_connection(c"requester1", c"write");
+                }
                 CommandType::ERASE => {
                     entry.set_ctx(req);
-                    return ssd_os_get_connection(c"requester1", c"erase")
-                },
+                    return ssd_os_get_connection(c"requester1", c"erase");
+                }
             }
         }
         None => {
@@ -105,23 +88,17 @@ fn pipe_start(entry: *mut lring_entry) -> *mut pipeline {
 }
 
 fn ring(entry: *mut lring_entry) -> ::core::ffi::c_int {
-    
-    // println!("BACK");
-    #[cfg(feature = "debug")]
-    ssd_os_sleep(1);
-
-    let res = lring_entry::new(entry).unwrap();
+    let Some(res) = lring_entry::new(entry) else {
+        return 0;
+    };
     let Some(req) = res.get_ctx_as_mut::<Request>() else {
         return 0;
     };
-
-    // println!("BACK1");
 
     // stop timer
     req.end_timer();
     let workload = WORKLOAD_GENERATOR.get_mut();
     workload.request_returned += 1;
-    
 
     #[cfg(feature = "debug")]
     {
--- src/apps/pipeline_per_cmd/pipes/write.rs
+++ src/apps/pipeline_per_cmd/pipes/write.rs
@@ -4,12 +4,10 @@ use crate::allocator::semaphore_alloc::SemaphoreAllocator;
 use crate::apps::pipeline_per_cmd::connectors::requester::WORKLOAD_GENERATOR;
 use crate::bbt::bbt::BadBlockTable;
 use crate::bindings::mem::MemoryRegion;
-use crate::bindings::safe::ssd_os_sleep;
 use crate::l2p::l2p::L2pMapper;
-use crate::media_manager::media_manager::{Geometry, MediaManager};
+use crate::media_manager::media_manager::MediaManager;
 use crate::provisioner::provisioner::Provisioner;
 use crate::shared::addresses::PhysicalBlockAddress;
-use crate::shared::core_local_cell::CoreLocalCell;
 use crate::shared::macros::ensure_unique;
 use crate::shared::semaphore::Semaphore;
 use crate::{make_stage_static, println};
@@ -33,45 +31,20 @@ make_stage_static!(write_mm, init_mm, exit, mm_context_handler);
 fn init_l2p() -> ::core::ffi::c_int {
     ensure_unique!();
 
-    #[cfg(feature = "debug")]
-    {
-        ssd_os_sleep(1);
-        println!("WRITE: INIT: L2P STAGE");
-    }
-
     let mem_region = MemoryRegion::new_from_cpu(2);
-    println!("{:?}", mem_region.free_start);
-    println!("{:?}", mem_region.end);
-
     L2P_ALLOC.init(mem_region.free_start.cast(), mem_region.end.cast());
     L2P_MAPPER.init(L2pMapper::new(&L2P_ALLOC));
     let n_requests = WORKLOAD_GENERATOR.get().get_n_requests();
     let mut l2p_map = L2P_MAPPER.lock();
     l2p_map.prepare_for_benchmark(n_requests);
 
-    // #[cfg(feature = "benchmark")]
-    // {
-    // let l2p_map = l2p_mapper.get_mut();
-    // l2p_map.prepare_for_benchmark(n_requests);
-    // }
-
-    // l2p_mapper.map(0x0, 0x1234);
-    // l2p_mapper.map(0x2, 0x5555);
-    // for i in 0..128 {
-    //     let i = i as u32;
-    //     l2p_mapper.map(i, i);
-    // }
     0
 }
 
 fn init_prov() -> ::core::ffi::c_int {
     ensure_unique!();
 
-    // ssd_os_sleep(1);
-    // println!("WRITE: INIT: PROV STAGE");
     let mem_region = MemoryRegion::new_from_cpu(3);
-    println!("{:?}", mem_region.free_start);
-    println!("{:?}", mem_region.end);
     PROV_ALLOC.init(mem_region.free_start.cast(), mem_region.end.cast());
 
     let geo = WORKLOAD_GENERATOR.get().get_geo();
@@ -85,11 +58,7 @@ fn init_prov() -> ::core::ffi::c_int {
 fn init_mm() -> ::core::ffi::c_int {
     ensure_unique!();
 
-    // ssd_os_sleep(1);
-    // println!("WRITE: INIT: MM STAGE");
     let mem_region = MemoryRegion::new_from_cpu(4);
-    println!("{:?}", mem_region.free_start);
-    println!("{:?}", mem_region.end);
 
     MM_ALLOC.init(mem_region.free_start.cast(), mem_region.end.cast());
     MM.init(MediaManager::new(&MM_ALLOC));
@@ -103,10 +72,6 @@ fn exit() -> ::core::ffi::c_int {
 fn prov_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
     ensure_unique!();
 
-    // ssd_os_sleep(1);
-    // println!("WRITE: PROV STAGE");
-    // We just propagete the context here.
-
     let req: &mut Result<Request, RequestError> = unsafe {
         context
             .cast::<Result<Request, RequestError>>()
@@ -115,10 +80,6 @@ fn prov_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::
     };
 
     if let Ok(request) = req {
-        // println!("L2P_WRITE_STAGE: {:?}", request);
-        // Modify the value behind the context pointer
-        // request.physical_addr = PROVISIONER.get_mut().provision_page().unwrap().into();
-
         let Ok(ppa) = PROVISIONER.lock().provision_page() else {
             println!("COULD NOT PROVISION!");
             return null_mut();
@@ -132,12 +93,6 @@ fn prov_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::
 fn l2p_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
     ensure_unique!();
 
-    // ssd_os_sleep(1);
-    // println!("WRITE: L2P STAGE");
-    // We just propagete the context here.
-
-    // TEST WE CAN GET IT
-    // let res = L2P_MAPPER.get_mut().lookup(0x1);
     let req: &mut Result<Request, RequestError> = unsafe {
         context
             .cast::<Result<Request, RequestError>>()
@@ -146,47 +101,16 @@ fn l2p_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c
     };
 
     if let Ok(request) = req {
-        // println!("L2P_WRITE_STAGE: {:?}", request);
-        // Modify the value behind the context pointer
         L2P_MAPPER
             .lock()
             .map(request.logical_addr, request.physical_addr.unwrap());
     }
 
-    // let Some(res) = L2P_MAPPER.get_mut().map() else {
-    //     return context;
-    // };
-
-    // println!("RES {:?}", res);
-
-    // let req : &mut Result<Request, RequestError> =  unsafe { context.cast::<Result<Request, RequestError>>().as_mut().unwrap() };
-
-    // if let Ok(request) = req {
-    //     // println!("L2P_WRITE_STAGE: {:?}", request);
-    //     // Modify the value behind the context pointer
-    //     request.physical_addr = Some(L2P_MAPPER.get_mut().lookup(request.logical_addr).unwrap());
-    // }
     context
 }
 
 fn mm_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
     ensure_unique!();
-    // ssd_os_sleep(1);
-
-    // println!("WRITE: MM STAGE");
-    // let req = context as *mut Result<Request, RequestError>;
-    // let req : &mut Result<Request, RequestError> =  unsafe { context.cast::<Result<Request, RequestError>>().as_mut().unwrap() };
-
-    // if let Ok(request) = req {
-    //     // println!("L2P_WRITE_STAGE: {:?}", request);
-    //     // Modify the value behind the context pointer
-    //     request.data = MM.get_mut().execute_request(request, None).unwrap();
-    // }
-
-    // println!("REQUESTER TO L2P STAGE: {:?}", unsafe {*req});
-
-    // We just propagete the context here.
-
     let req: &mut Result<Request, RequestError> = unsafe {
         context
             .cast::<Result<Request, RequestError>>()
@@ -195,8 +119,6 @@ fn mm_context_handler(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_
     };
 
     if let Ok(request) = req {
-        // println!("L2P_READ_STAGE: {:?}", request);
-        // Modify the value behind the context pointer
         let Ok(data) = MM.lock().execute_request(request) else {
             if let Some(_pba) = request.physical_addr {
                 //TODO: only do this because ssd_os does not support LLVM 64-bit operations,
