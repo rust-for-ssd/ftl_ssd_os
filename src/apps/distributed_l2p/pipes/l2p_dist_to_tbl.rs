cd03661f6524d4c954d91333e6203ad0d5407084
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-05-02 17:13:05 +0200

    add l2p distributer to l2p table pipe

---
+++ src/apps/distributed_l2p/pipes/l2p_dist_to_tbl.rs
@@ -0,0 +1,51 @@
+use core::ffi::CStr;
+
+use crate::{
+    apps::distributed_l2p::connectors::l2p::l2p_tables::N_TABLES, make_stage_static,
+    shared::macros::ensure_unique,
+};
+
+const STAGE_NAMES: [&CStr; N_TABLES] = [
+    dist_l2p_stage0.get_name(),
+    dist_l2p_stage1.get_name(),
+    dist_l2p_stage2.get_name(),
+    dist_l2p_stage3.get_name(),
+];
+
+make_stage_static!(dist_l2p_stage0, init, exit, context_handler_dist_l2p0);
+make_stage_static!(dist_l2p_stage1, init, exit, context_handler_dist_l2p1);
+make_stage_static!(dist_l2p_stage2, init, exit, context_handler_dist_l2p2);
+make_stage_static!(dist_l2p_stage3, init, exit, context_handler_dist_l2p3);
+
+fn init() -> ::core::ffi::c_int {
+    0
+}
+
+fn exit() -> ::core::ffi::c_int {
+    0
+}
+
+#[inline(never)]
+#[unsafe(no_mangle)]
+fn context_handler_dist_l2p0(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
+    ensure_unique!();
+    context
+}
+#[inline(never)]
+#[unsafe(no_mangle)]
+fn context_handler_dist_l2p1(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
+    ensure_unique!();
+    context
+}
+#[inline(never)]
+#[unsafe(no_mangle)]
+fn context_handler_dist_l2p2(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
+    ensure_unique!();
+    context
+}
+#[inline(never)]
+#[unsafe(no_mangle)]
+fn context_handler_dist_l2p3(context: *mut ::core::ffi::c_void) -> *mut ::core::ffi::c_void {
+    ensure_unique!();
+    context
+}
